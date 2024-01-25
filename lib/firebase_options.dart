5d9ab1bb1b146537bbe140082660139de3fd0a7e
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-01-25 18:11:12 +0800

    refactor: add necessary API keys for the project

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -9,31 +9,17 @@ class DefaultFirebaseOptions {
   static FirebaseOptions get androidPlatform {
     return const FirebaseOptions(
       // Replace these with actual values from Firebase console when setting up Firebase for Android app
-      apiKey: "AIzaSyDDGkg8ZG26GT2j_wTlOR5Xj2JjLZh8AY0",
-      appId: "1:777878936021:android:980072560145bcc0df855c",
-      messagingSenderId: "777878936021",
-      projectId: "student-event-calendar-dce10",
+      apiKey: "AIzaSyCmes3OFjY2rZyZTj0Ir7gw4Jl7k61iISU",
+      appId: "1:97362035713:android:33fe4bbda188a3140eac00",
+      messagingSenderId: "97362035713",
+      projectId: "bscs-android-chat",
     );
   }
-
-  // Firebase configuration for iOS platform
-  static FirebaseOptions get iosPlatform {
-    return const FirebaseOptions(
-      // Replace these with actual values from Firebase console when setting up Firebase for iOS app
-      apiKey: "AIzaSyDDGkg8ZG26GT2j_wTlOR5Xj2JjLZh8AY0",
-      appId: "1:777878936021:ios:d06f0a15a482fe84df855c",
-      messagingSenderId: "777878936021",
-      projectId: "student-event-calendar-dce10",
-    );
-  }
-
   // Getter for current platform's Firebase configuration
   static FirebaseOptions get currentPlatform {
     // Checks on which platform the app currently runs and returns appropriate Firebase options
     if (Platform.isAndroid) {
       return androidPlatform;
-    } else if (Platform.isIOS) {
-      return iosPlatform;
     } else {
       // If the current platform is not web, nor Android, nor iOS, an UnsupportedError is thrown
       throw UnsupportedError('Unsupported platform');
